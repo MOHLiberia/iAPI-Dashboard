9e6de302f9ddc6cdca7d2eb78db10966d6f984ef
redmondking <lanba24@gmail.com>
2024-11-25 05:21:19 +0000

    created basic auth pages header and footer

---
+++ app/components/auth/header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header class="p-2">
+  <nuxt-link to="/" class="flex items-center gap-2 w-fit">
+    <img src="/assets/images/moh-logo.svg" alt="MOH Logo" class="w-20"/>
+    <div class="text-lg">
+      <p>Interoperability API</p>
+      <p>Dashboard</p>
+    </div>
+  </nuxt-link>
+
+</header>
+</template>
+
