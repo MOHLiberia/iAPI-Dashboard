b3f67e51fd2a0d7c7e4458ac4fc5e46d1daa0e25
redmondking <lanba24@gmail.com>
2024-12-19 20:52:30 +0000

    updated auth store to integrate with auth layout

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -5,12 +5,12 @@ import {useAuthStore} from "@/stores/auth";
 export default defineNuxtRouteMiddleware((to, from) => {
     const useAuth = useAuthStore()
 
-    if (to.fullPath.includes('/auth') && useAuth.authenticated) {
-        return navigateTo('/dashboard')
-    }
-
-    if (to.fullPath.includes('/dashboard')  && !useAuth.authenticated) {
-        console.log(to.path)
-        return navigateTo('/auth/login')
-    }
+    // if (to.fullPath.includes('/auth') && useAuth.authenticated) {
+    //     return navigateTo('/dashboard')
+    // }
+    //
+    // if (to.fullPath.includes('/dashboard')  && !useAuth.authenticated) {
+    //     console.log(to.path)
+    //     return navigateTo('/auth/login')
+    // }
 })
