7ad1b63ec84cfdd25e078c62dc6d41c1fe57db40
redmondking <lanba24@gmail.com>
2024-12-19 20:47:48 +0000

    simplified layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,25 @@
+<script setup lang="ts">
+import {useRoute} from "#app";
+
+const openSideMenu = ref(true);
+const route = useRoute()
+
+const router = useRouter();
+</script>
+
 <template>
-  <div>
-    <div>
-      <header/>
-      <sidebar/>
+  <div class="flex flex-col">
+    <default-header v-on:openSideMenu="openSideMenu=!openSideMenu"/>
+    <div class="flex flex-1">
+      <sideMenu :open="openSideMenu"/>
+      <div class="flex flex-col flex-1 overflow-x-hidden">
+        <div class="flex-1 p-4 overflow-x-hidden overflow-y-auto">
+          <breadcrumbs/>
+          <div class="text-lg mb-4">{{route.name}}</div>
+          <slot/>
+        </div>
+        <default-footer/>
+      </div>
     </div>
-    <slot/>
-    <footer/>
   </div>
 </template>
-
-
-
-<script setup lang="ts">
-</script>
