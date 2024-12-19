f3484fae63ebab1558e9e23317f2f7f44f140cfa
redmondking <lanba24@gmail.com>
2024-12-19 20:50:46 +0000

    chore: refactored code for better readability

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,8 @@
 <template>
   <Toast position="bottom-right" />
-  <NuxtLayout class="dark:bg-slate-950 bg-slate-100">
+  <NuxtLayout class="dark:bg-slate-950 bg-slate-50 h-screen">
     <NuxtPage/>
   </NuxtLayout>
 </template>
-
 <script setup lang="ts">
 </script>
