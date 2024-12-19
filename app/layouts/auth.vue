9e0629ef61f5219246f3c1132f42d511e35fff02
redmondking <lanba24@gmail.com>
2024-12-19 20:52:24 +0000

    updated auth store to integrate with auth layout

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col">
     <AuthHeader/>
     <div class="flex-1 flex flex-col items-center justify-center p-3 md:px-0">
       <slot/>
--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import {useUserStore} from "~/stores/user";
+import { useUserStore } from "./user";
 
 interface LoginCredentials {
     usernameOrEmail: string;
@@ -9,17 +9,13 @@ interface LoginCredentials {
 export const useAuthStore = defineStore('auth', () => {
     const config = useRuntimeConfig()
     const toastService = usePVToastService()
-    const useUser = useUserStore()
+    const user = useUserStore()
 
     const accessToken = useCookie('access-token')
     const refreshToken = useCookie('refresh-token')
 
-
     const authenticated = ref(false)
     const pending = ref(false)
-
-
-
 
     const login = async ({usernameOrEmail, password}: { usernameOrEmail: string; password: string }) => {
         pending.value = true
@@ -29,14 +25,13 @@ export const useAuthStore = defineStore('auth', () => {
                 'api-key': config.public.apiKey.toString(),
                 'content-type': 'application/json'
             },
-            ignoreResponseErrors: true,
             body: JSON.stringify({usernameOrEmail, password}),
         })
-            .then(async (res) => {
-                // useUser.user = res.data?.
-                console.log(res.data)
+            .then(async (res: any) => {
                 accessToken.value = res.data?.accessToken
                 refreshToken.value = res.data?.refreshToken
+                user.setUser(res.data?.user)
+                authenticated.value = true
                 await navigateTo('/dashboard')
             })
             .catch((err) => {
@@ -52,6 +47,12 @@ export const useAuthStore = defineStore('auth', () => {
             })
     }
 
-    return {login, authenticated, pending}
+    const logout = async () => {
+    console.log('logout')
+    }
+
+    return {login, authenticated, pending, logout}
+}, {
+    // persist: true,
 })
 
