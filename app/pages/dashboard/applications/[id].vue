14cafddea0b5ccceacd489c02272a371377c08a0
redmondking <lanba24@gmail.com>
2024-12-19 20:50:38 +0000

    created application details page

---
+++ app/pages/dashboard/applications/[id].vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+definePageMeta({
+  name: 'Application Details',
+  breadcrumb: 'Application Details',
+  icon: 'tabler:app-window-filled'
+})
+
+const route = useRoute()
+const config = useRuntimeConfig()
+
+const {data: application, pending} = useFetch(`${config.public.apiUrl}/application/${route.params.id}`, {
+  method: 'GET',
+  headers: {
+    'api-key': config.public.apiKey.toString(),
+    'content-type': 'application/json'
+  },
+})
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+      <Button size="small" class="w-fit">
+        <Icon name="tabler:edit"/>
+        <span>Edit</span>
+      </Button>
+    <Card>
+      <template #title>
+        Hello
+      </template>
+    </Card>
+  </div>
+</template>
