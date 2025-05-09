875e2c40cea50c96268292722ade5b02bda04e79
redmondking <lanba24@gmail.com>
2025-05-09 16:49:04 +0000

    update: added persistence and removed get and set user functions

--- app/stores/user.ts
+++ app/stores/user.ts
@@ -2,20 +2,15 @@ import {defineStore} from 'pinia'
 
 export const useUserStore = defineStore('user', () =>{
     const user = ref({})
-    const accessToken = useCookie('access_token')
-    const refreshToken = useCookie('refresh_token')
 
     const reset = () => {
         user.value = {}
     }
 
-    const setUser = (data: any) => {
-        user.value = data
+    return { user, reset }
+}, {
+    persist: {
+        storage: localStorage,
+        pick: ['user'],
     }
-
-    const getUser = () => {
-        return user.value
-    }
-
-    return { setUser, reset, getUser, accessToken, refreshToken }
 })
