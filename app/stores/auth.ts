930016defb6f9c4f9c6b0b082af7e9484a22de19
redmondking <lanba24@gmail.com>
2025-05-09 16:47:26 +0000

    update: expanded logout function with toast message and create a check and refresh token and added localstorage persistance

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -4,10 +4,12 @@ import { useUserStore } from "./user";
 export const useAuthStore = defineStore('auth', () => {
     const config = useRuntimeConfig()
     const toastService = usePVToastService()
-    const user = useUserStore()
+    const useUser = useUserStore()
 
-    const accessToken = useCookie('access_token')
-    const refreshToken = useCookie('refresh_token')
+    const accessToken = useCookie('access_token', {maxAge: 60 * 60})
+    const refreshToken = useCookie('refresh_token', {maxAge: 6 * 60 * 60})
+
+    const countdownInterval = ref()
 
     const authenticated = ref(false)
     const pending = ref(false)
@@ -23,11 +25,17 @@ export const useAuthStore = defineStore('auth', () => {
             body: JSON.stringify({usernameOrEmail, password}),
         })
             .then(async (res: any) => {
-                accessToken.value = res.data?.accessToken
-                refreshToken.value = res.data?.refreshToken
-                user.setUser(res.data?.user)
+                setTokens({ accessToken: res.data?.accessToken, refreshToken: res.data?.refreshToken })
+                useUser.user = res.data?.user
                 authenticated.value = true
+                toastService.add({
+                    severity: 'success',
+                    summary: res.status || 'Success',
+                    detail: res.message,
+                    life: 5000,
+                })
                 await navigateTo('/dashboard')
+                return
             })
             .catch((err) => {
                 toastService.add({
@@ -43,18 +51,50 @@ export const useAuthStore = defineStore('auth', () => {
     }
 
     const logout = async () => {
+        pending.value = true
         await $fetch(`${config.public.apiUrl}/auth/logout`, {
             method: 'POST',
             headers: {
                 'api-key': config.public.apiKey.toString(),
                 'content-type': 'application/json',
-                'authorization': `Bearer ${user.accessToken}`
+                'authorization': `Bearer ${accessToken.value}`
             },
         })
-            .then(async () => {
-                user.reset()
+            .then(async (res: any) => {
                 authenticated.value = false
-                await navigateTo('/login')
+                await resetTokens()
+                toastService.add({
+                    severity: 'success',
+                    summary: res.status,
+                    detail: res.message || 'Something went wrong. If this continues, please get in touch with the ICT Unit.',
+                    life: 5000,
+                })
+                await navigateTo('/auth/login')
+            })
+            .catch((err) => {
+                toastService.add({
+                    severity: 'error',
+                    summary: err.data?.status || 'Error',
+                    detail: err.data?.message || 'Something went wrong. If this continues, please get in touch with the ICT Unit.',
+                    life: 5000,
+                })
+            }).finally(async () => {
+                useUser.reset()
+                pending.value = false
+            })
+    }
+
+    const refreshAccessToken = async () => {
+        await $fetch(`${config.public.apiUrl}/auth/refresh-token`, {
+            method: 'POST',
+            headers: {
+                'api-key': config.public.apiKey.toString(),
+                'content-type': 'application/json',
+                'authorization': `Bearer ${accessToken.value}`
+            },
+        })
+            .then(async (res: any) => {
+                setTokens({accessToken: res.data?.accessToken, refreshToken: res.data?.refreshToken})
             })
             .catch((err) => {
                 toastService.add({
@@ -66,8 +106,53 @@ export const useAuthStore = defineStore('auth', () => {
             })
     }
 
-    return {login, authenticated, pending, logout}
+    const checkAndRefreshTokens = async () => {
+        if (!accessToken.value) {
+            await resetTokens();
+            return;
+        }
+
+        if (!refreshToken.value) {
+            await resetTokens();
+            return;
+        }
+
+        if (!accessToken.value) {
+            await refreshAccessToken();
+        }
+    }
+
+    const setTokens = (tokens: { accessToken: string; refreshToken: string }) => {
+        accessToken.value = tokens.accessToken
+        refreshToken.value = tokens.refreshToken
+
+        startCountdown()
+    }
+
+    const resetTokens = async () => {
+        accessToken.value = null
+        refreshToken.value = null
+
+        clearInterval(countdownInterval.value)
+
+        await navigateTo('auth/login')
+    }
+
+    const startCountdown = () => {
+        if (countdownInterval.value) {
+            clearInterval(countdownInterval.value)
+        }
+
+        countdownInterval.value = setInterval(async () => {
+           await checkAndRefreshTokens()
+        }, 60 * 1000)
+    }
+
+    return {authenticated, pending, accessToken, refreshToken, login, logout, refreshAccessToken, checkAndRefreshTokens}
 }, {
-    // persist: true,
+    persist: {
+        storage: localStorage,
+        pick: ['authenticated'],
+    },
 })
 
