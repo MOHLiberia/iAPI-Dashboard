9b7aa529214dc2f762143485fbf343a295be6a55
redmondking <lanba24@gmail.com>
2025-05-09 16:51:39 +0000

    update: added @pinia-plugin-persistedstate/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,17 +11,17 @@ export default defineNuxtConfig({
     '@primevue/nuxt-module',
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt',
     '@nuxt/icon',
     '@nuxtjs/color-mode',
+    'pinia-plugin-persistedstate/nuxt',
   ],
   devServer: {
     port: 3001
   },
   runtimeConfig: {
     public: {
-      apiUrl: '',
-      apiKey: '',
+      apiUrl: 'localhost:3000/api/v1',
+      apiKey: '12345',
     }
   },
   primevue: {
@@ -29,4 +29,8 @@ export default defineNuxtConfig({
       from: '@/theme/myTheme.ts'
     },
   },
+  icon: {
+    provider: 'iconify',
+    serverBundle: 'local'
+  }
 })
