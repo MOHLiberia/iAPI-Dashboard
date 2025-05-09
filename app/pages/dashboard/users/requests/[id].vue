e7f08a730e2e89ff162c5881a91e1f630909fea5
redmondking <lanba24@gmail.com>
2025-05-09 16:28:08 +0000

    init: created page

---
+++ app/pages/dashboard/users/requests/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  label: "Account Request",
+  icon: "tabler:users-plus"
+})
+
+// const config = useRuntimeConfig()
+const route = useRoute()
+
+// const  {data: accountRequest, pending} = useFetch(`${config.public.apiUrl}/account-requests/${route.params.id}`, {
+//   method: 'GET',
+//   headers: {
+//     'api-key': config.public.apiKey.toString(),
+//   }
+// })
+</script>
+
+<template>
+
+</template>
