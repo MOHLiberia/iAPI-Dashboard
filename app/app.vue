c4811a681ba6f637d29caea3d1bf5d5b2add9707
redmondking <lanba24@gmail.com>
2025-05-09 16:27:06 +0000

    update: added confirmation dialog and added html headers

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,22 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
 <template>
+  <ConfirmDialog></ConfirmDialog>
   <Toast position="bottom-right" />
   <NuxtLayout class="dark:bg-slate-950 bg-slate-50 h-screen">
     <NuxtPage/>
   </NuxtLayout>
 </template>
-<script setup lang="ts">
-</script>
