5cc9194f5f0ebdda82c93c78974588e2382b5c14
redmondking <lanba24@gmail.com>
2025-05-09 16:53:48 +0000

    init: created page with table

---
+++ app/pages/dashboard/users/requests/index.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import {Form, type FormSubmitEvent} from "@primevue/forms";
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { z } from "zod";
+
+definePageMeta({
+  label: "Account Requests",
+  icon: "tabler:users-plus"
+})
+
+const confirm = useConfirm()
+const config = useRuntimeConfig()
+
+const {data: requests, pending} = useFetch(`${config.public.apiUrl}/account-request`, {
+  method: 'GET',
+  headers: {
+    'api-key': config.public.apiKey.toString(),
+    'content-type': 'application/json'
+  },
+})
+
+const cancel = () => {
+  confirm.require({
+    header: "Cancel",
+    message: "Are you sure you want to cancel?",
+    acceptProps: {
+      label: 'Yes',
+      severity: 'danger',
+    },
+    rejectProps: {
+      label: "Continue",
+      severity: "success",
+    },
+    accept: async () => {
+      await navigateTo("/dashboard/users")
+    },
+  })
+}
+</script>
+
+<template>
+  <div>
+    <DataTable :value="requests?.data" class="text-nowrap" :loading="pending" size="small" showGridlines>
+      <Column field="firstName" header="First Name"/>
+      <Column field="middleName" header="Middle Name"/>
+      <Column field="lastName" header="Last Name"/>
+      <Column field="email" header="Email"/>
+      <Column field="organization" header="Organization"/>
+      <Column field="department" header="Department" class="!w-fit"/>
+      <Column field="county.name" header="County Name" class="!w-fit"/>
+      <Column field="status" header="Status">
+        <template #body="slotProps">
+          <div class="flex justify-center">
+            <div class="w-4 h-4 rounded-full animate-pulse" :class="slotProps.data.firstName === 'Chris' ? 'bg-green-500' : 'bg-orange-500'"/>
+          </div>
+        </template>
+      </Column>
+      <Column header="Actions" body-style="{width: fit-content}" header-style="{max-width: fit-content}">
+        <template #body="slotProps">
+          <div class="flex justify-center">
+            <NuxtLink :to="`/dashboard/users/requests/${slotProps.data.id}`">
+              <Button label="View" size="small"/>
+            </NuxtLink>
+          </div>
+        </template>
+      </Column>
+      <template #footer>There are <span class="font-semibold">{{ requests ? requests?.data.length : 0 }}</span> application request(s). </template>
+    </DataTable>
+  </div>
+  {{requests}}
+</template>
