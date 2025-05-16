ec1db4c3a3cfb66a61cfd1e7e5c90a050b3fa99a
redmondking <lanba24@gmail.com>
2025-05-16 13:22:57 +0000

    migrating to use menuList component

--- app/components/mobileMenu.vue
+++ app/components/mobileMenu.vue
@@ -20,22 +20,23 @@ const openMenu = ref(false)
         </div>
       </div>
     </template>
-      <div class="overflow-y-auto space-y-2 h-full">
-        <div class="rounded flex border dark:border-slate-800 hover:dark:bg-slate-700 p-2" v-for="list in mainMenuList">
-          <NuxtLink :to="list.to" class="flex-1 flex gap-2 items-center text-lg">
-            <Icon :name="list.icon" size="1.25rem"/>
-            <span>{{list.name}}</span>
-          </NuxtLink>
-        </div>
-      </div>
-    <template #footer>
-        <NuxtLink class="w-full rounded border dark:border-slate-800 dark:hover:bg-slate-800/35 p-2 group flex items-center gap-2" to="/dashboard/account">
-          <div class="h-10 w-10 rounded-full border-slate-800 bg-green-300"/>
-          <div class="flex flex-col text-sm">
-            <div>Calvin Coleman</div>
-            <div>ICT Director</div>
-          </div>
-        </NuxtLink>
-    </template>
+    <menuList :menuList="mainMenuList"/>
+<!--      <div class="overflow-y-auto space-y-2 h-full">-->
+<!--        <div class="rounded flex border dark:border-slate-800 hover:dark:bg-slate-700 p-2" v-for="list in mainMenuList">-->
+<!--          <NuxtLink :to="list.to" class="flex-1 flex gap-2 items-center text-lg">-->
+<!--            <Icon :name="list.icon" size="1.25rem"/>-->
+<!--            <span>{{list.name}}</span>-->
+<!--          </NuxtLink>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    <template #footer>-->
+<!--        <NuxtLink class="w-full rounded border dark:border-slate-800 dark:hover:bg-slate-800/35 p-2 group flex items-center gap-2" to="/dashboard/account">-->
+<!--          <div class="h-10 w-10 rounded-full border-slate-800 bg-green-300"/>-->
+<!--          <div class="flex flex-col text-sm">-->
+<!--            <div>Calvin Coleman</div>-->
+<!--            <div>ICT Director</div>-->
+<!--          </div>-->
+<!--        </NuxtLink>-->
+<!--    </template>-->
   </drawer>
 </template>
